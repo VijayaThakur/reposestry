6825a599106af0d137c47034cb14eac1cae0e7f6
VijayaThakur <99168500+VijayaThakur@users.noreply.github.com>
2022-04-12 12:57:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var yellow,red,green;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+  fill("black");
+  rect(100, 50, 100, 300); 
+  rect(140, 350, 20, 300);
+}
+
+
+function draw() {
+  
+  red = new Signal(150, 100, "red");
+  red.appear();
+  
+  //Create objects for Green and yellow lights :
+}
+    
+
